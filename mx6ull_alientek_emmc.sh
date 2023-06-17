5c04cd5f5e647e4751ffad9efe7d4399f386f7c3
ligh_honor_ubuntu_16 <lightlay80@163.com>
2023-06-17 12:40:54 +0800

    alientek_emmc linux

---
+++ mx6ull_alientek_emmc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alientek_emmc_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
+
+
